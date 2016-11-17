bd9ba5234abf6a620195ef02e6a65b36089705e3
Watchanan <tmwatchanan@gmail.com>
2016-11-17 13:23:49 +0700

    ! Made authentication using data from database * with explicit sending parameters * <- should fix this

--- nok-cinema-web/Controllers/AuthenticationController.cs
+++ nok-cinema-web/Controllers/AuthenticationController.cs
@@ -5,11 +5,17 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 using nok_cinema_web.Models;
+using nok_cinema_web.ViewModels;
 
 namespace nok_cinema_web.Controllers
 {
     public class AuthenticationController : Controller
     {
+        CinemaEntities db = new CinemaEntities();
+        EMPLOYEE employee = new EMPLOYEE();
+        PERSON person = new PERSON();
+        NowLogin nowlogin = new NowLogin();
+
         // GET: Authentication
         public ActionResult Login()
         {
@@ -17,19 +23,58 @@ namespace nok_cinema_web.Controllers
         }
 
         [HttpPost]
-        public ActionResult DoLogin(UserDetails u)
+        public ActionResult DoLogin(PERSON p)
         {
-            EmployeeBusinessLayer bal = new EmployeeBusinessLayer();
-            if (bal.IsValidUser(u))
+
+
+            IQueryable<PERSON> query1 = from tmp in db.PERSON
+                                        where tmp.USERNAME.Equals(p.USERNAME) & tmp.PASSWORD.Equals(p.PASSWORD)
+                                        select tmp;
+
+            foreach (PERSON temp in query1)
             {
-                FormsAuthentication.SetAuthCookie(u.Username, false);
-                return RedirectToAction("Index", "EMPLOYEEs");
+                person.CITIZENID = temp.CITIZENID;
+            }
+
+            IQueryable<EMPLOYEE> query2 = from tmp in db.EMPLOYEE
+                                          where tmp.PERSON.CITIZENID.Equals(person.CITIZENID)
+                                          select tmp;
+            if (query1.Count() == 1 && query2.Count() == 1)
+            {
+                foreach (PERSON _p in query1.ToList())
+                {
+                    person.FNAME = _p.USERNAME;
+                    person.LNAME = _p.LNAME;
+                    person.CITIZENID = _p.CITIZENID;
+                    person.GENDER = _p.GENDER;
+                    person.EMAIL = _p.EMAIL;
+                    person.USERNAME = _p.USERNAME;
+                    person.PASSWORD = _p.PASSWORD;
+                }
+                foreach (EMPLOYEE _e in query2.ToList())
+                {
+                    employee.CITIZENID = _e.CITIZENID;
+                    employee.SALARY = _e.SALARY;
+                    employee.JOBPOSITION = _e.JOBPOSITION;
+                }
+                nowlogin = new NowLogin(employee, person);
+                FormsAuthentication.SetAuthCookie(nowlogin.USERNAME, false);
+
+                return RedirectToAction("Index", "Home", nowlogin);
             }
             else
             {
                 ModelState.AddModelError("CredentialError", "Invalid Username or Password");
                 return View("Login");
             }
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            nowlogin.Cleanup();
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
         }
     }
 }

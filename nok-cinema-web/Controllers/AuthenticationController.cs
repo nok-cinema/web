1642eb5e9afa7214112e56ae9ddda7c771be97b4
Watchanan <tmwatchanan@gmail.com>
2016-11-20 17:09:21 +0700

    + Added cookie expiration checking to Login action method

--- nok-cinema-web/Controllers/AuthenticationController.cs
+++ nok-cinema-web/Controllers/AuthenticationController.cs
@@ -27,20 +27,27 @@ namespace nok_cinema_web.Controllers
             //    return RedirectToAction("ShowInformation", "People");
             //}
             //return View();
-            if (Request.Cookies[FormsAuthentication.FormsCookieName] == null)
+            HttpCookie authCookie = Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie == null)
                 return View();
             else
             {
-                string userName = FormsAuthentication.Decrypt(Request.Cookies[FormsAuthentication.FormsCookieName].Value).Name;
-                var peopleBLL = new PeopleBLL();
-                person = peopleBLL.GetPersonByCookie(userName);
-                var employeesBLL = new EmployeesBLL();
-                employee = employeesBLL.GetEmployeeByCitizenId(person.CITIZENID);
+                FormsAuthenticationTicket ticket = FormsAuthentication.Decrypt(authCookie.Value);
+                DateTime expiration = ticket.Expiration;
+                if (expiration < System.DateTime.Now)
+                    return View();
+                else
+                {
+                    string userName = FormsAuthentication.Decrypt(Request.Cookies[FormsAuthentication.FormsCookieName].Value).Name;
+                    var peopleBLL = new PeopleBLL();
+                    person = peopleBLL.GetPersonByCookie(userName);
+                    var employeesBLL = new EmployeesBLL();
+                    employee = employeesBLL.GetEmployeeByCitizenId(person.CITIZENID);
 
-                userProfile = new UserProfile(employee, person);
-                FormsAuthentication.SetAuthCookie(userProfile.USERNAME, false);
-                TempData["UserProfileData"] = userProfile;
-                return RedirectToAction("ShowInformation", "People");
+                    userProfile = new UserProfile(employee, person);
+                    TempData["UserProfileData"] = userProfile;
+                    return RedirectToAction("ShowInformation", "People");
+                }
             }
         }
 

5b25d3463064de7ea44b02949c3e5afc1d980e68
Ratpon <rattapon.kaewpinjai@gmail.com>
2016-12-07 17:32:26 +0700

    + fixed Date Format

--- nok-cinema-web/BLL/ShowtimeBLL.cs
+++ nok-cinema-web/BLL/ShowtimeBLL.cs
@@ -45,7 +45,7 @@ namespace nok_cinema_web.BLL
                     if (same)
                     {
                         showtime.SHOWDATES.Add(showtimeTuple.SHOWDATE);
-                        showtime.DATES = showtimeTuple.SHOWDATE.ToShortDateString();
+                        showtime.DATES = showtimeTuple.SHOWDATE.ToString("dd/MM/yyyy");
                         showtime.TIMES.Add(showtimeTuple.SHOWDATE.ToShortTimeString());
                         showtime.MOVIEID = showtimeTuple.MOVIEID;
                         showtime.THEATREID = showtimeTuple.THEATREID;
@@ -56,7 +56,7 @@ namespace nok_cinema_web.BLL
 
                         showtime = new ShowtimeViewModel();
                         showtime.SHOWDATES.Add(showtimeTuple.SHOWDATE);
-                        showtime.DATES = showtimeTuple.SHOWDATE.ToShortDateString();
+                        showtime.DATES = showtimeTuple.SHOWDATE.ToString("dd/MM/yyyy");
                         showtime.TIMES.Add(showtimeTuple.SHOWDATE.ToShortTimeString());
                         showtime.MOVIEID = showtimeTuple.MOVIEID;
                         showtime.THEATREID = showtimeTuple.THEATREID;
@@ -64,7 +64,7 @@ namespace nok_cinema_web.BLL
 
                     if (i == showtimeQuery.Count())
                     {
-                        showtime.DATES = showtimeTuple.SHOWDATE.ToShortDateString();
+                        showtime.DATES = showtimeTuple.SHOWDATE.ToString("dd/MM/yyyy");
                         showtimes.Add(showtime);
                     }
 

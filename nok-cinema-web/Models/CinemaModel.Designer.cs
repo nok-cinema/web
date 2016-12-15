218fd9b20707cab28d4dacb4703838dbbce3f840
Watchanan <tmwatchanan@gmail.com>
2016-12-15 21:52:56 +0700

    ! Regenerated CinemaModel - Adjusted homestyle css

--- nok-cinema-web/Models/CinemaModel.Designer.cs
+++ nok-cinema-web/Models/CinemaModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\CPE\Database\Project\webDEV\nok-cinema-web\Models\CinemaModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Watchanan\documents\visual studio 2015\Projects\nok-cinema-web\nok-cinema-web\Models\CinemaModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
